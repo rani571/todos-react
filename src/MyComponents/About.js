17263df03322e43ade514b1c8b66dc96da1734d9
rani571 <ranikumari4356867@gmail.com>
2022-04-30 02:31:28 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h4>This is the about page.</h4>
+      <p>Todo list offers a way to increase productivity, stopping you from forgetting things, helps priorities tasks, manage tasks effectively, use time wisely and improve time management as well as workflow.</p>
+    </div>
+  )
+}
+
+export default About
